d374c6065e24da9308ed6faeed37680ce1754459
Ahmed Malek <ahmedmalekvic@mail.ru>
2019-12-27 00:09:18 +0200

    Updates

---
+++ precache-manifest.7fc9f8c2da6b97eaf95bc4ba6796a061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593f8298843cd4f6c81f52bf5eca3a6f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "011b60dfa08d38f027f7",
+    "url": "/monster-rolodex/static/css/main.128171ee.chunk.css"
+  },
+  {
+    "revision": "32631a4503210d894319",
+    "url": "/monster-rolodex/static/js/2.86f1e2f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-rolodex/static/js/2.86f1e2f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "011b60dfa08d38f027f7",
+    "url": "/monster-rolodex/static/js/main.e8b2bf9b.chunk.js"
+  },
+  {
+    "revision": "7841a3ebe0962832862e",
+    "url": "/monster-rolodex/static/js/runtime-main.ed6168f4.js"
+  }
+]);
